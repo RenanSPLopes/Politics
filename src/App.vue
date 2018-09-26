45c8e48fe4c9fbf21331818949f592179a22456a
Renan <renandasilva16@gmail.com>
2018-09-26 09:01:04 -0300

    Criando arcabouço

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <b-navbar type="dark" variant="primary">
+      <b-navbar-brand> Deu BayBlade </b-navbar-brand>
+    </b-navbar> 
+    <router-view/>
+  </div>
+</template>
+
---
+++ src/views/PoliticalParty.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="political-party container">
+    <h1 class="text-center mb-5 mt-5"> Cadastro Partido Politico</h1>
+    <b-form>
+        <b-form-group id="name-group-input"
+          class="container ml-5 mb-4"
+          label="Nome do Partido: "
+          label-for="name-input">
+        <b-form-input id="name-input" 
+            class="w-50 p-3" 
+            v-model="politicalParty.name"
+            placeholder="Digite o nome do Partido">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="abbreviation-group-input"
+          class="container ml-5 mb-4"
+          label="Sigla :"
+          label-for="abbreviation-input">
+            <b-form-input id="abbreviation-input"
+              class="w-25"
+              v-model="politicalParty.abbreviation"
+              placeholder="Digite a sigla">
+            </b-form-input>
+        </b-form-group>
+        <b-form-group id="legend-group-input"
+          class="container ml-5 mb-4"
+          label="Número da Legenda :"
+          label-for="legend-input">
+            <b-form-input id="abbreviation-input"
+              class="w-25"
+              v-model="politicalParty.legend"
+              placeholder="Digite o número da legenda">
+            </b-form-input>
+        </b-form-group>
+        <b-form-group id="description-group-input"
+          class="container ml-5 mb-4"
+          label="Descrição"
+          label-for="description-input">
+          <b-form-textarea id="description-textarea"
+            class="w-50 p-3"
+            placeholder="Digite a descrição do partido"
+            v-model="politicalParty.description"
+            :rows="3"
+            :max-rows="10">
+          </b-form-textarea>
+        </b-form-group>
+        <div class="container">
+          <b-button type="submit" class="ml-5 mr-2" variant="primary"> Cadastrar </b-button>
+          <b-button type="reset" variang="danger"> Cancelar </b-button>
+        </div>
+    </b-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'political-party',
+  data() {
+    return {
+      politicalParty : {
+        name: '',
+        abbreviation: '',
+        legend: '',
+        description: ''
+      }
+    };
+  }
+}
+</script>
